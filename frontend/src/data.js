713c97594e5705902965d3a457a9774a778ae267
Yan <yankaiquecosta.yk@gmail.com>
2020-08-31 09:16:22 -0300

    Render Products (JSX)

---
+++ frontend/src/data.js
@@ -0,0 +1,11 @@
+export default{
+    products:[
+        {name:"Slim Shirt",category:'Shirts',image:"/images/d1.jpg",price:60,brand:"Nike",rating:3.5,numReviews:10},
+        {name:"Fit Shirt",category:'Shirts',image:"/images/d1.jpg",price:70,brand:"Adidas",rating:4.7,numReviews:10},
+        {name:"Best pants",category:'Pants',image:"/images/d1.jpg",price:60,brand:"Puma",rating:2.5,numReviews:10},
+        {name:"Nice Shirt",category:'Shirts',image:"/images/d1.jpg",price:95,brand:"Oakley",rating:4.5,numReviews:10},
+        {name:"Slim Shirt",category:'Pants',image:"/images/d1.jpg",price:80,brand:"Nike",rating:4.5,numReviews:10},
+        {name:"Slim Shirt",category:'Shirts',image:"/images/d1.jpg",price:60,brand:"Nike",rating:4.5,numReviews:10},
+
+    ]
+}
