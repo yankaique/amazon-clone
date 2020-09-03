bd4245c202c3b47583549b57beae96cbcef57093
Yan <yankaiquecosta.yk@gmail.com>
2020-09-03 06:07:24 -0300

    Manage State With Redux (Redux)

---
+++ frontend/src/store.js
@@ -0,0 +1,12 @@
+import {createStore, combineReducers, applyMiddleware, compose} from 'redux';
+import {productListReducer} from './reducers/productReducers';
+import thunk from 'redux-thunk';
+
+const initialState = {};
+const reducer = combineReducers({
+    productList:productListReducer
+});
+
+const composeEnhacer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducer, initialState, composeEnhacer(applyMiddleware(thunk)));
+export default store;
